a878264980580b525b0dd49b48b9968fc173bc0a
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-09-10 16:29:21 -0500

    Updating App

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List usersData = [];
+  String uri = 'https://rickandmortyapi.com/api/character?page=1';
 
   getUsersRickAndMortyAPI() async {
     Map data;
 
-    http.Response res = await http
-        .get(Uri.parse('https://rickandmortyapi.com/api/character?page=1'));
+    http.Response res = await http.get(Uri.parse(uri));
     //debugPrint(res.body);
 
     data = json.decode(res.body);
@@ -40,46 +40,73 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Rick and Morty API'),
+        title: Text('Rick and Morty'),
         backgroundColor: Colors.indigo[900],
       ),
-      body: ListView.builder(
-        itemCount: usersData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Text(
-                      "${usersData[index]["id"]}",
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.w500),
-                    ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "At first glance, Rick and Morty looks like just another animated situational family comedy, like Family Guy or The Simpsons. Theoretically, that's true. When sociopathic scientist and estranged grandfather Rick returns to his daughter Beth’s life, he brings with him all sorts of family drama. Watching Beth's family of four navigate life with their self-centered, cynical, and narcissistic grandfather is, indeed, often very funny. In a sea of sad quotes on the internet, Rick and Morty quotes are the hilariously sardonic lines you need to buoy your dark heart.",
+                  style: TextStyle(
+                    fontSize: 20.0,
                   ),
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(usersData[index]["image"]),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text(
-                        "${usersData[index]["name"]} ${usersData[index]["species"]}",
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w900,
-                            color: Colors.black45),
-                      ),
-                    ),
-                  )
-                ],
+                ),
               ),
             ),
-          );
-        },
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: usersData.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Wrap(
+                  children: [
+                    Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text(
+                                "${usersData[index]["id"]}",
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                            CircleAvatar(
+                              backgroundImage:
+                                  NetworkImage(usersData[index]["image"]),
+                            ),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: Padding(
+                                padding: const EdgeInsets.all(12.0),
+                                child: Text(
+                                  "${usersData[index]["name"]} ${usersData[index]["species"]}",
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w900,
+                                      color: Colors.black45),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
