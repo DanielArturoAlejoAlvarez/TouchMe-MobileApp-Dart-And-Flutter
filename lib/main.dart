1a0a9de9a4b59d625c4bf0c086ac716e2c95a488
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2021-09-10 12:11:03 -0500

    Creating HomePage class extends StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,15 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(MaterialApp());
 }
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+}
